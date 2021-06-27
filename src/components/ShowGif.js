74ea4bef2f82d0903975c56536a03da3270a88e1
Lautaro Linquiman <lylinquiman@gmail.com>
2021-06-27 01:18:58 -0300

    Update github actions

--- src/components/ShowGif.js
+++ src/components/ShowGif.js
@@ -8,8 +8,8 @@ export const ShowGif = ({title, url}) => {
     return (
         <div className='card animate__animated animate__pulse'>
             { !state && <p className="animate__animated animate__flash animate__infinite">Cargando...</p>}
-            <img src={url} onLoad={() => setstate(true)}/>
-            <p className='title'>{title.trim().length == 0 ? "Sin titulo" : title}</p>
+            <img src={url} onLoad={() => setstate(true)} alt={title}/>
+            <p className='title'>{title.trim().length === 0 ? "Sin titulo" : title}</p>
         </div>
     )
 }
